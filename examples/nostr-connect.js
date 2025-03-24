98ffb3dbc30ff3b83017e11b671105f5546af8bc
Yuki Kishimoto <yukikishimoto@protonmail.com>
2025-03-24 08:26:38 +0100

    Fix nostr-connect example
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

--- examples/nostr-connect.js
+++ examples/nostr-connect.js
@@ -1,5 +1,4 @@
-const { Keys, Client, NostrSigner, NostrConnect, NostrConnectURI, loadWasmAsync, initLogger, LogLevel, Duration } = require("../");
-const {EventBuilder} = require("../pkg/nostr_sdk_js");
+const { Keys, Client, NostrSigner, NostrConnect, NostrConnectURI, loadWasmAsync, initLogger, LogLevel, Duration, EventBuilder } = require("../");
 
 async function main() {
     await loadWasmAsync();
@@ -7,13 +6,13 @@ async function main() {
     initLogger(LogLevel.info());
 
     // App keys
-    let appKeys = Keys.parse("...");
+    let appKeys = Keys.parse("nsec1lxxqxj4z2ru8p7d45m48thlcfaj594fw4jaj8tfp8zpxylh4ahsscs359t");
 
     // Remote signer (NIP46)
-    let uri = NostrConnectURI.parse("bunker://..");
+    let uri = NostrConnectURI.parse("bunker://...");
     let timeout = Duration.fromSecs(60);
-    let nip46 = new NostrConnect(uri, appKeys, timeout);
-    let signer = NostrSigner.nip46(nip46);
+    let connect = new NostrConnect(uri, appKeys, timeout);
+    let signer = NostrSigner.nip46(connect);
 
     // Compose client and add relays
     let client = new Client(signer);
@@ -21,9 +20,12 @@ async function main() {
     await client.addRelay("wss://nos.lol");
     await client.addRelay("wss://nostr.oxtr.dev");
     await client.connect();
-    
-    let builder = EventBuilder.textNote("My first text note from rust-nostr WASM with NIP46 signer!", []);
-    await client.sendEventBuilder(builder);
+
+    let builder = EventBuilder.textNote("Text note from rust-nostr WASM with NIP46 signer!");
+    let output = await client.sendEventBuilder(builder);
+    console.log("ID:", output.id.toBech32());
+    console.log("Sent to:", output.success);
+    console.log("Failed to send to:", output.failed);
 }
 
 main();
